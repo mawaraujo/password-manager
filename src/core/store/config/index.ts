2db84f055608b30fdb7d64822a1d904359834b9d
Mauro Araujo <mauro.araujo@smg.com.ar>
2021-09-27 19:01:40 -0300

    encrypt global state added

--- src/core/store/config/index.ts
+++ src/core/store/config/index.ts
@@ -1,5 +1,6 @@
 import { combineReducers, createStore, compose } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
+import { encryptTransform } from 'redux-persist-transform-encrypt';
 import storage from 'redux-persist/lib/storage';
 
 import { notificationReducer } from '../reducers/notifications.reducer';
@@ -18,6 +19,14 @@ const persistConfig = {
   key: 'root',
   storage: storage,
   whitelist: ['tags', 'passwords'],
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_SECRET_STATE_PASS ?? 'YOUR_SECRET_PASSWORD',
+      onError: function(err: any) {
+        console.log('ERROR ON ENCRYPT THE STATE', err);
+      },
+    }),
+  ],
 };
 
 const reducers = combineReducers({
